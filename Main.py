17221f9d5498f2de8e18e7202711d3ec066ba424
LinuKKx <kika.kalolo@outlook.com>
2025-10-18 10:42:02 +1300

    VSCode

---
+++ Main.py
@@ -0,0 +1,23 @@
+print()
+print("Welcome to Kika's short but simple interactive code.")
+print()
+
+name = input('What is your name? ')
+print()
+print("Hello", name)
+print()
+num1 = 1
+num2 = 2
+num3 = 3
+NumLingo = int(input("How many languages can you speak? "))
+print()
+
+if NumLingo == num1:
+    print('1 is more than enough', name)
+    print()
+elif NumLingo == num2:
+    print('Very impressive', name)
+    print()
+else:
+    print('OMG! You are on fire', name)
+    print()
